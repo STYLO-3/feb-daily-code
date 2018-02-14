ecd51fbae4a05bb0591f7f57b6b74728560bf0d7
STYLO-3 <35541285+STYLO-3@users.noreply.github.com>
2018-02-14 14:25:06 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include  <iostream>
+using namespace std;
+
+
+int main() {
+	char intput;
+	cout << " press y ";
+	cin >> intput; 
+	while (intput == 'y') {
+		cout << "<3" << endl;
+		system("pause");
+	}
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
